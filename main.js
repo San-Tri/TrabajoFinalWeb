c88d6f9f49fee9baeb2496f5cb1720a062db9314
San-Tri <1473024@senati.pe>
2023-04-22 21:53:52 -0500

    avanse.1.3

--- main.js
+++
@@ -1,6 +0,0 @@
-let listElements = document.querySelectorAll('.list_button--click')
-listElements.forEach(listElements => 
-    {listElements.addEventListener('click', ()=>{
-        listElements.classList.toggle('arrow');
-    })
-});
